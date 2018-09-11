78bb33ac0bf7c4aabdceef4472fd06f4d866aabe
Tim Jacomb <tim.jacomb@hmcts.net>
2018-09-11 16:52:58 +0100

    Use az cli to get token

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 Run 
 ```
 yarn install
-export COSMOS_AUTH_KEY=<read access key get from portal
 ./run.sh
 ```
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+export COSMOS_AUTH_KEY=$(az cosmosdb list-read-only-keys --resource-group pipelinemetrics-database-prod --name pipeline-metrics --query primaryReadonlyMasterKey -o tsv)
 node index.js | jq -r '.[] | "\(.count)\t\(.stage)"'
